867925cb7080fda040f7ae5570801ebbc55e73b1
sybrix <dsmith03@gmail.com>
2015-09-01 00:49:51 -0400

    commit

---
+++ framework/src/main/java/sybrix/easygsp2/EasyGSPServlet.java
@@ -0,0 +1,7 @@
+package sybrix.easygsp2;
+
+/**
+ * Created by dsmith on 8/15/15.
+ */
+public class EasyGSPServlet {
+}
---
+++ framework/src/main/java/sybrix/easygsp2/data/XMLSerializer.java
@@ -0,0 +1,7 @@
+package sybrix.easygsp2.data;
+
+/**
+ * Created by dsmith on 8/16/15.
+ */
+public interface XMLSerializer {
+}
