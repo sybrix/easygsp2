e42d5dab8a5bf199742cacbefef6ca854c2a5378
Sybrix <dsmith@sybrix.com>
2018-01-28 12:03:42 -0500

    .

--- webapp/src/main/groovy/testapp/models/Marker.groovy
+++
@@ -1,9 +0,0 @@
-package testapp.models
-
-/**
- * Created by dsmith on 11/27/15.
- */
-class Marker {
-        String position
-        String markerPosition
-}
--- webapp/src/main/resources/routes.groovy
+++
@@ -1,6 +0,0 @@
-// add httpMethod, path, controller
-
-//add 'get', '/good/test', 'testapp.controllers.DefaultController', 'test'
-add 'get', '/', 'testapp.controllers.DefaultController', 'index', ["int"]
-//add 'get', '/', 'testapp.controllers.DefaultController', 'test', []
-//add 'get', '/good/testx', 'testapp.controllers.DefaultController', 'testx'
